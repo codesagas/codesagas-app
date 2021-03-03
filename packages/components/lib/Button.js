6f409d4d135f934d506e2f2a672de33647bb544f
Brian Bartholomew <me@bcbrian.com>
2021-03-03 07:11:42 -0700

    fix: made the button different
    affects: @codesagas/components

--- packages/components/lib/Button.js
+++ packages/components/lib/Button.js
@@ -1,14 +1,25 @@
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 import * as React from "react";
 export function Button(props) {
-    return (React.createElement("button", { style: {
-            backgroundColor: "rebeccapurple",
-            color: "white",
+    return (React.createElement("button", __assign({ style: {
+            background: "rebeccapurple",
+            color: "hotpink",
             borderRadius: "4px",
             border: "none",
             padding: "8px 12px",
             fontSize: "16px",
             textTransform: "uppercase",
             letterSpacing: "2px",
-            margin: "10px 0",
-        } }, props.children));
+            cursor: "pointer",
+        } }, props)));
 }
